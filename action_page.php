b3b68865fd9bbff277baf55feed331947f7de841
jjbb1221 <41380360+jjbb1221@users.noreply.github.com>
2019-01-22 21:25:15 -0600

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -6,7 +6,7 @@
 
 <?php
 // define variables and set to empty values
-$uname = "";
+?uname = "daytonhudson","aidensankey","gabenunez";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $uname = test_input($_POST["Password"]);
@@ -21,7 +21,7 @@ function test_input($data) {
 ?>
 
 <?php
-if $uname = ("daytonhudson","gabenunez","aidansankey") {
+if ?uname = ("daytonhudson","gabenunez","aidansankey") {
 <a href="url">https://jjbb1221.github.io/capitolcorps/home.html</a>;
 } else {
 <a href="url">https://jjbb1221.github.io/capitolcorps/loginfailed.html</a>;
